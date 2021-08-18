ef020915128b22cdfb590ec787fd45f798dd766f
zsofikassai <65860961+zsofikassai@users.noreply.github.com>
2021-08-18 13:13:31 +0200

    Create testclass for inbox class

---
+++ SaintSender.Core.Tests/InboxTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SaintSender.Core.Models;
+
+namespace SaintSender.Core.Tests
+{
+    [TestFixture]
+    public class InboxTests
+    {
+        [Test]
+        public void ListMails_UserNamePasswordAdded_ReturnEmailList()
+        {
+            // Arrange
+            var connection = new Inbox();
+            // Act
+            var Emails = connection.MailList()
+            // Assert
+            Assert.AreEqual(Emails, Emails);
+        }
+    }
+}
